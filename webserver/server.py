b2b0a3f47af35e0dbb3512485f9f1a2817077bdd
Ubuntu <azakem@UbuntuServerPass.f1lhsdwdrr2efb2u4ebuyccepd.bx.internal.cloudapp.net>
2016-04-04 22:08:38 +0000

    Fixing typo in SQL query in route for /drivers

--- webserver/server.py
+++ webserver/server.py
@@ -124,7 +124,7 @@ def drivers():
   print user_id
   # for reference, column indices are: 0=name, 1=phone, 2=date, 3=time, 4=type, 5=distance, 6=pick_addr, 7=drop_add, 8=est_amount
   query = "SELECT P.name, P.phone, to_char(T.date, \'YYYY-MM-DD\') AS date, to_char(T.time, \'HH:MI:SS\') AS time, T.type, T.distance, T.pick_addr, T.drop_addr, T.est_amount" + \
-    "FROM Trips T, Passengers P WHERE T.driver={} T.passenger = P.uid AND T.status!=\'completed\' ORDER BY T.date, T.time".format(user_id)
+    " FROM Trips T, Passengers P WHERE T.driver={} T.passenger = P.uid AND T.status!=\'completed\' ORDER BY T.date, T.time".format(user_id)
   cursor = g.conn.execute(query.rstrip())
   reservations = []
   for row in cursor: 
